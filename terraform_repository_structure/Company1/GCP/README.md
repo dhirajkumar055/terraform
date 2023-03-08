666b61e4ae0a6dd6d66aecd70812f14f6604398c
Dheeraj Kumar Chopra <dheeraj.chopra@games24x7.com>
2023-03-08 08:29:55 +0530

    Part 1: Terraform Repository Structure : Environment Added

--- terraform_repository_structure/Company1/GCP/README.md
+++
@@ -1,1 +0,0 @@
-Company1 GCP
--- terraform_repository_structure/create_folders.sh
+++ terraform_repository_structure/create_folders.sh
@@ -2,12 +2,17 @@
 
 companies=(Company1 Company2 Company3 Company4 Company5)
 providers=(AWS GCP Azure Digital_Ocean)
+environment=(Dev Staging QA Pre_Prod Prod DR)
 for i in $(echo ${companies[@]})
 do
   for j in $(echo ${providers[@]})
   do
-    echo "$i $j"
-    mkdir -p $i/$j
-    echo "$i $j" >$i/$j/README.md
+    for k in $(echo ${environment[@]})
+    do
+      echo "$i $j $k"
+      mkdir -p $i/$j/$k
+      rm $i/$j/README.md
+      echo "$i $j $k" >$i/$j/$k/README.md
+    done
   done
 done
