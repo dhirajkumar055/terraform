1f9eec9f70090d654f84ee7b073ad04e9f7c4413
Dheeraj Kumar Chopra <dheeraj.chopra@games24x7.com>
2023-03-11 08:46:06 +0530

    Part 2: Subnet creation : forbidden

--- terraform_repository_structure/gcp/company5/prod/3-subnet.tf
+++ terraform_repository_structure/gcp/company5/prod/3-subnet.tf
@@ -1,16 +1,16 @@
-resource "google_compute_subnetwork" "dheeraj-private-subnet"{
-  name = "dheeraj-private-subnet"
-  ip_cidr_range = "10.0.0.0/18"
-  region = "asia-south1"
-  network = "default"
-  private_ip_google_access = true
-  secondary_ip_range {
-    range_name = "dheeraj-company5-prod-k8s-pod-range"
-    ip_cidr_range = "10.48.0.0/18"
-  }
-  secondary_ip_range {
-    range_name = "dheeraj-company5-prod-k8s-service-range"
-    ip_cidr_range = "10.52.0.0/20"
-  }
-}
-
+#resource "google_compute_subnetwork" "dheeraj-private-subnet"{
+#  name = "dheeraj-private-subnet"
+#  ip_cidr_range = "10.0.0.0/18"
+#  region = "asia-south1"
+#  network = "default"
+#  private_ip_google_access = true
+#  secondary_ip_range {
+#    range_name = "dheeraj-company5-prod-k8s-pod-range"
+#    ip_cidr_range = "10.48.0.0/18"
+#  }
+#  secondary_ip_range {
+#    range_name = "dheeraj-company5-prod-k8s-service-range"
+#    ip_cidr_range = "10.52.0.0/20"
+#  }
+#}
+#
