bb50e3d862f4a4fb731e381c30d85e067c4b993f
Dheeraj Kumar Chopra <dheeraj.chopra@games24x7.com>
2023-03-11 10:24:43 +0530

    Part 2: Kubernetes : IP address allocation commented

--- terraform_repository_structure/gcp/company5/prod/2-kubernetes.tf
+++ terraform_repository_structure/gcp/company5/prod/2-kubernetes.tf
@@ -36,14 +36,14 @@ resource "google_container_cluster" "k8s-cluster"{
   workload_identity_config{
     workload_pool = "dataloop-candidate-environment.svc.id.goog"
   }
-  ip_allocation_policy{
+  #ip_allocation_policy{
     #cluster_secondary_range_name = "k8s-pod-range"
     #cluster_secondary_range_name = "10.160.0.0/21"
     #services_secondary_range_name = "10.160.8.0/21"
     #services_secondary_range_naem = "k8s-service-range"
-    cluster_secondary_range_name = "default"
-    services_secondary_range_name = "default"
-  }
+    #cluster_secondary_range_name = "default"
+    #services_secondary_range_name = "default"
+  #}
   private_cluster_config{
     enable_private_nodes = true
     enable_private_endpoint = false
