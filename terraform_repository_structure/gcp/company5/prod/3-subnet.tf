d08db0d1d2ef64648b569b758588953683883ac0
Dheeraj Kumar Chopra <dheeraj.chopra@games24x7.com>
2023-03-11 08:44:10 +0530

    Part 2: Subnet : added default vpc network

--- terraform_repository_structure/gcp/company5/prod/3-subnet.tf
+++ terraform_repository_structure/gcp/company5/prod/3-subnet.tf
@@ -2,7 +2,7 @@ resource "google_compute_subnetwork" "dheeraj-private-subnet"{
   name = "dheeraj-private-subnet"
   ip_cidr_range = "10.0.0.0/18"
   region = "asia-south1"
-  network = default 
+  network = "default"
   private_ip_google_access = true
   secondary_ip_range {
     range_name = "dheeraj-company5-prod-k8s-pod-range"
