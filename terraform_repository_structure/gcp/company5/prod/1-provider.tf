f29907d9d537b39d965d662aca0e7cd147bc5ea7
Dheeraj Kumar Chopra <dheeraj.chopra@games24x7.com>
2023-03-12 19:19:09 +0530

    Part 2: Kubernetes : K8s provider and load config to false to use public kubeconfig

--- terraform_repository_structure/gcp/company5/prod/1-provider.tf
+++ terraform_repository_structure/gcp/company5/prod/1-provider.tf
@@ -8,6 +8,11 @@ terraform {
       source  = "hashicorp/google"
       version = "4.56.0"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.18.1"
+      load_config_file = false
+    }
   }
 }
 
