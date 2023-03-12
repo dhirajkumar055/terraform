f5e2aec81bef60b4c9dfa27b307b8ad4b182cb36
Dheeraj Kumar Chopra <dheeraj.chopra@games24x7.com>
2023-03-12 17:40:50 +0530

    Part 2: Kubernetes : Node count to 3

--- terraform_repository_structure/gcp/company5/prod/3-node-pools.tf
+++ terraform_repository_structure/gcp/company5/prod/3-node-pools.tf
@@ -8,7 +8,7 @@ resource "google_container_node_pool" "general"{
   }
   node_config {
     preemptible = false
-    machine_type = "e2-small"
+    machine_type = "e2-medium"
     labels = {
       role = "general"
     }
